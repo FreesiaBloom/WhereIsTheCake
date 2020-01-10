647619feb58a49354c6e95693e16c1a7ce141b72
jasinaga <agata.jasinska@tieto.com>
2020-01-10 20:56:16 +0100

    adding gun

---
+++ NewProject/Assets/AutoDestruction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestruction : MonoBehaviour
+{
+    public float life_time = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, life_time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
